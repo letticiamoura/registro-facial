6d28fe5e81fd81abfb8fe4cbb2fb4757ced78f06
Leticia Moura <leeh.mourah62@gmail.com>
2024-08-30 11:12:16 -0300

    feat: Criação do Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,5 @@
+export default function Footer() {
+    return(
+        <footer className="py-3 flex justify-center items-end bottom-0 text-center text-orange-500 font-medium text-xs">&copy;Rede de Ensino Alfatec</footer>
+    )
+}
