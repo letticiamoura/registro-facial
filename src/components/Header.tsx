f3621beeaba049ef4bc0edb47e37b6c44bd8c740
Leticia Moura <leeh.mourah62@gmail.com>
2024-08-30 11:11:33 -0300

    feat: Criação do Cabeçario

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return(
+        <header className="rounded-bl-[50px] rounded-br-[50px] bg-orange-500 w-full">
+
+            <h1 className="py-5 text-center text-5xl font-extrabold font-serif text-zinc-50">Alfatec</h1>
+
+        </header>
+    )
+}
