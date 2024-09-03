3acba004b4b1f34630cf4183152731d6c3a88bfd
Leticia Moura <leeh.mourah62@gmail.com>
2024-09-03 15:57:02 -0300

    feat: Utilizando react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Register from "./pages/Register";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import HomeAdmin from "./pages/admin/HomeAdmin";
 
 export default function App() {
   return (
-      <div>
-        <Register />
-      </div>
+      <BrowserRouter basename="/registro-facial">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<HomeAdmin />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
   )
 }
