0d68f6b8e8c59233da3961d15d692746ec9ea0ed
Leticia Moura <leeh.mourah62@gmail.com>
2024-11-15 09:40:15 -0300

    style: Alterando cores

--- src/pages/admin/GroupRegister.tsx
+++ src/pages/admin/GroupRegister.tsx
@@ -2,29 +2,29 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 interface Group {
-  id?: number; // ID do grupo, opcional se estiver criando um novo
-  name: string; // Nome do grupo
-  description: string; // Descrição do grupo
-  initialDate: string; // Data inicial
-  endDate: string; // Data final
-  initialTime: string; // Horário inicial
-  endTime: string; // Horário final
-  monday: boolean; // Se o grupo se reúne na segunda-feira
-  tuesday: boolean; // Se o grupo se reúne na terça-feira
-  wednesday: boolean; // Se o grupo se reúne na quarta-feira
-  thursday: boolean; // Se o grupo se reúne na quinta-feira
-  friday: boolean; // Se o grupo se reúne na sexta-feira
-  saturday: boolean; // Se o grupo se reúne no sábado
-  sunday: boolean; // Se o grupo se reúne no domingo
+  id?: number;
+  name: string;
+  description: string;
+  initialDate: string;
+  endDate: string;
+  initialTime: string;
+  endTime: string;
+  monday: boolean;
+  tuesday: boolean;
+  wednesday: boolean;
+  thursday: boolean;
+  friday: boolean;
+  saturday: boolean;
+  sunday: boolean;
 }
 
 interface GroupRegisterProps {
-  group: Partial<Group>; // Permite que o objeto seja parcialmente preenchido
-  onClose: () => void; // Função chamada ao fechar o modal
-  editMode: boolean; // Indica se está em modo de edição
+  group: Partial<Group>;
+  onClose: () => void;
+  editMode: boolean;
 }
 
-const GroupRegister: React.FC<GroupRegisterProps> = ({ group, onClose, editMode }) => {
+export default function GroupRegister ({ group, onClose, editMode }: GroupRegisterProps) {
   const [formData, setFormData] = useState<Group>({
     name: '',
     description: '',
@@ -43,14 +43,14 @@ const GroupRegister: React.FC<GroupRegisterProps> = ({ group, onClose, editMode 
 
   useEffect(() => {
     if (editMode && group) {
-      setFormData({ ...group } as Group);
+      setFormData((prev) => ({ ...prev, ...group } as Group));
     }
   }, [editMode, group]);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, type, checked, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
+    const { name, type, value, checked } = e.target;
+    setFormData((prev) => ({
+      ...prev,
       [name]: type === 'checkbox' ? checked : value,
     }));
   };
@@ -58,104 +58,85 @@ const GroupRegister: React.FC<GroupRegisterProps> = ({ group, onClose, editMode 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      // Enviar dados do grupo
-      const response = await axios.post('http://localhost:8080/groups', formData);
+      const url = editMode ? `http://localhost:8080/groups/${formData.id}` : 'http://localhost:8080/groups';
+      const method = editMode ? axios.put : axios.post;
+      const response = await method(url, formData);
       console.log('Grupo salvo:', response.data);
-      onClose(); // Fecha o modal após salvar
+      onClose();
     } catch (error) {
       console.error('Erro ao salvar grupo:', error);
     }
   };
 
+  const daysOfWeek = [
+    { name: 'monday', label: 'Segunda-feira' },
+    { name: 'tuesday', label: 'Terça-feira' },
+    { name: 'wednesday', label: 'Quarta-feira' },
+    { name: 'thursday', label: 'Quinta-feira' },
+    { name: 'friday', label: 'Sexta-feira' },
+    { name: 'saturday', label: 'Sábado' },
+    { name: 'sunday', label: 'Domingo' },
+  ];
+
   return (
-    <div className="modal p-6 bg-white rounded-lg shadow-lg">
+    <div className="modal p-6 bg-white rounded-lg shadow-lg fixed top-[10vh] w-[80vw] left-[10vw]">
       <h2 className="text-2xl font-semibold mb-4">{editMode ? 'Editar Grupo' : 'Cadastrar Grupo'}</h2>
       <form onSubmit={handleSubmit} className="space-y-4">
-        <div>
-          <label className="block text-sm font-medium text-gray-700">Nome</label>
-          <input
-            type="text"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-            className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-orange-300"
-          />
-        </div>
-        <div>
-          <label className="block text-sm font-medium text-gray-700">Descrição</label>
-          <input
-            type="text"
-            name="description"
-            value={formData.description}
-            onChange={handleChange}
-            required
-            className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-orange-300"
-          />
-        </div>
+        {['name', 'description'].map((field) => (
+          <div key={field}>
+            <label className="block text-sm font-medium text-gray-700">
+              {field === 'name' ? 'Nome' : 'Descrição'}
+            </label>
+            <input
+              type="text"
+              name={field}
+              // value={formData[field as keyof Group] || ''}
+              onChange={handleChange}
+              required
+              className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-orange-300"
+            />
+          </div>
+        ))}
+
         <div className="grid grid-cols-2 gap-4">
-          <div>
-            <label className="block text-sm font-medium text-gray-700">Data Inicial</label>
-            <input
-              type="date"
-              name="initialDate"
-              value={formData.initialDate}
-              onChange={handleChange}
-              required
-              className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-orange-300"
-            />
-          </div>
-          <div>
-            <label className="block text-sm font-medium text-gray-700">Data Final</label>
-            <input
-              type="date"
-              name="endDate"
-              value={formData.endDate}
-              onChange={handleChange}
-              required
-              className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-orange-300"
-            />
-          </div>
-          <div>
-            <label className="block text-sm font-medium text-gray-700">Horário Inicial</label>
-            <input
-              type="time"
-              name="initialTime"
-              value={formData.initialTime}
-              onChange={handleChange}
-              required
-              className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-orange-300"
-            />
-          </div>
-          <div>
-            <label className="block text-sm font-medium text-gray-700">Horário Final</label>
-            <input
-              type="time"
-              name="endTime"
-              value={formData.endTime}
-              onChange={handleChange}
-              required
-              className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-orange-300"
-            />
-          </div>
+          {[
+            { name: 'initialDate', label: 'Data Inicial', type: 'date' },
+            { name: 'endDate', label: 'Data Final', type: 'date' },
+            { name: 'initialTime', label: 'Horário Inicial', type: 'time' },
+            { name: 'endTime', label: 'Horário Final', type: 'time' },
+          ].map(({ name, label, type }) => (
+            <div key={name}>
+              <label className="block text-sm font-medium text-gray-700">{label}</label>
+              <input
+                type={type}
+                name={name}
+                // value={formData[name as keyof Group] || ''}
+                onChange={handleChange}
+                required
+                className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-orange-300"
+              />
+            </div>
+          ))}
         </div>
-        <div className="mt-4">
+
+        <div>
           <label className="block text-sm font-medium text-gray-700">Dias da Semana</label>
-          <div className="flex flex-wrap">
-            {['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'].map((day) => (
-              <label key={day} className="inline-flex items-center mr-4">
+          <div className="flex flex-wrap mt-2">
+            {daysOfWeek.map(({ name, label }) => (
+              <label key={name} className="inline-flex items-center mr-4">
                 <input
                   type="checkbox"
-                  name={day}
-                  checked={formData[day as keyof Group]}
+                  name={name}
+                  // checked={formData[name as keyof Group]}
                   onChange={handleChange}
                   className="h-4 w-4 text-orange-500 border-gray-300 rounded focus:ring focus:ring-orange-300"
                 />
-                <span className="ml-2 text-sm text-gray-700 capitalize">{day}</span>
+                <span className="ml-2 text-sm text-gray-700">{label}</span>
               </label>
             ))}
           </div>
         </div>
+
         <div className="flex justify-end space-x-2 mt-4">
           <button
             type="button"
@@ -175,5 +156,3 @@ const GroupRegister: React.FC<GroupRegisterProps> = ({ group, onClose, editMode 
     </div>
   );
 };
-
-export default GroupRegister;
