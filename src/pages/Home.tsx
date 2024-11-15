9caa99a0d5abd2eaa0a98f1e02e02a27c9db4ddb
Leticia Moura <leeh.mourah62@gmail.com>
2024-11-15 07:47:55 -0300

    style: Alterando cor de fundo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,12 +4,14 @@ import { RiAdminFill } from "react-icons/ri";
 import { FaUser } from "react-icons/fa";
 
 export default function Home() {
+
     const navigate = useNavigate();
     
     const handleLogin = () => navigate('/login');
     const handleRegister = () => navigate('/register');
 
-    const classname="h-40 w-3/4 md:w-1/6 p-3 text-2xl font-medium font-serif text-white bg-orange-500 rounded-md text-center hover:bg-orange-400 transition-colors hover:scale-105 duration-200";
+    const classname="h-40 w-3/4 md:w-1/6 p-3 text-2xl font-medium font-serif text-white bg-blue rounded-md text-center hover:bg-ligth-blue transition-colors hover:scale-105 duration-200";
+
     return(
         <Layout>
             <div className="px-5 pt-52 flex gap-5 justify-center items-center">
