d5acd533450f2fda6f8135f847b99d4241ec3361
Leticia Moura <leeh.mourah62@gmail.com>
2024-11-15 07:43:48 -0300

    style: Alterando cor de fundo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,18 @@
 import { Link } from "react-router-dom";
 
 export default function Header() {
-    return(
-        <Link to="/">
-            <header className="rounded-bl-[15px] rounded-br-[15px] shadow-md bg-orange-500 w-full">
 
-                <h1 className="py-5 text-center text-5xl font-extrabold font-serif text-zinc-50">Alfatec</h1>
+    return(
+        
+        <Link to="/">
+
+            <header className="rounded-bl-[35px] rounded-br-[35px] shadow-md bg-blue w-full">
+
+                <h1 className="py-5 text-center text-5xl font-extrabold font-serif text-zinc-200">Alfatec</h1>
 
             </header>
+
         </Link>
+
     )
 }
